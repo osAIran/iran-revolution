1ba8a565be5d1219499d34547b086beedd953b23
We Are Jina Amini <we.are.jina.amini@proton.me>
2022-12-02 20:21:52 +0100

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer" style={{background:"transparent"}}>
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by the Iranian Revolutionary Scholars</h3>
+          <h3>Designed and Developed by Iranian Revolutionary Scholars</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} WLF</h3>
