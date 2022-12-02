271a8e8cef1fb569a78d9680e5054dfff0013389
We Are Jina Amini <we.are.jina.amini@proton.me>
2022-12-02 21:43:24 +0100

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,17 +11,17 @@ function Footer() {
   let date = new Date();
   let year = date.getFullYear();
   return (
-    <Container fluid className="footer" style={{background:"transparent"}}>
+    <Container fluid className="footer" style={{ background: "transparent" }}>
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Iranian Revolutionary Scholars</h3>
+          <h3>Designed and Developed by <span className="purple-bold">Mahsa Amini's</span> Army</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} WLF</h3>
         </Col>
         <Col md="4" className="footer-body">
           {/* <ul className="footer-icons"> */}
-            {/* <li className="social-icons">
+          {/* <li className="social-icons">
               <a
                 href="https://github.com/soumyajit4419"
                 style={{ color: "white" }}
